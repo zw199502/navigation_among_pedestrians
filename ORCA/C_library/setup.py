84464e54195cbab2cff42e90abecf8b2da2c1d3b
wei.zhu <45971057+zw199502@users.noreply.github.com>
2022-11-12 18:10:42 +0900

    Delete ORCA/C_library directory

--- ORCA/C_library/setup.py
+++
@@ -1,7 +0,0 @@
-# from setuptools import setup
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(name = 'path_plan', 
-      ext_modules = cythonize("motion_plan_lib.pyx")
-     )
