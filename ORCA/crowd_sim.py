194d7e06d4e86d638574d541b1ab44387415090a
wei.zhu <45971057+zw199502@users.noreply.github.com>
2022-11-13 16:50:20 +0900

    Update crowd_sim.py

--- ORCA/crowd_sim.py
+++ ORCA/crowd_sim.py
@@ -344,6 +344,7 @@ class CrowdSim:
                 self.ax.add_artist(human_circle)
             self.ax.add_artist(plt.Circle(self.robot.get_position(), self.robot.radius, fill=True, color='r'))
             self.ax.add_artist(plt.Circle((self.robot.gx, self.robot.gy), self.robot.radius, fill=True, color='g'))
+            plt.text(-4.5, -4.5, str(round(self.global_time, 2)), fontsize=20)
             # x, y, theta = self.robot.px, self.robot.py, self.robot.theta
             # dx = cos(theta)
             # dy = sin(theta)
